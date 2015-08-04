743d43cacb73c685edda7501523fd3ae0478d177
Tiim <tim.bachmann@cad.ch>
2015-08-04 19:43:21 +0200

    Adds debug output.

--- src/main/java/ch/tiim/trainingmanager/update/VersionCheckTask.java
+++ src/main/java/ch/tiim/trainingmanager/update/VersionCheckTask.java
@@ -1,11 +1,16 @@
 package ch.tiim.trainingmanager.update;
 
+import ch.tiim.log.Log;
 import javafx.concurrent.Task;
 
 public class VersionCheckTask extends Task<Boolean> {
+    private static final Log LOGGER = new Log(VersionCheckTask.class);
 
     @Override
     protected Boolean call() throws Exception {
+        LOGGER.info("Version " + VersionChecker.getCurrentVersion() + " --> " +
+                VersionChecker.getRemoteVersion());
+
         return VersionChecker.isNewVersionAvailable();
     }
 }
