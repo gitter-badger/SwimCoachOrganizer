93573d209f91a5e7b407cbf2c924984ce6d065ad
Tiim <tim.bachmann@cad.ch>
2015-08-13 22:18:40 +0200

    Adds test for TableSets insert, update and update with utf8.

--- src/test/java/ch/tiim/sco/database/TableSetsTest.java
+++ src/test/java/ch/tiim/sco/database/TableSetsTest.java
@@ -9,13 +9,25 @@ import org.junit.Test;
 import java.io.IOException;
 import java.sql.SQLException;
 
+@SuppressWarnings("SpellCheckingInspection")
 public class TableSetsTest {
+
+    private Set set;
 
     private DatabaseController db;
     private TableSets sets;
 
     @Before
     public void setup() throws SQLException {
+        set = new Set(
+                -1,
+                "TestName",
+                "Content",
+                1, 2, 3, 99,
+                null, null,
+                "Test Note",
+                10, true
+        );
         db = new DatabaseController(":memory:");
         sets = db.getTblSet();
     }
@@ -27,16 +39,28 @@ public class TableSetsTest {
 
     @Test
     public void testInsert() throws SQLException {
-        Set s = new Set(
-                -1,
-                "TestName",
-                "Content",
-                1, 2, 3, 99,
-                null, null,
-                "Test Note",
-                10, true
-        );
-        sets.addSet(s);
-        Assert.assertEquals(s, sets.getAllSets().get(0));
+        sets.addSet(set);
+        Assert.assertEquals(set, sets.getAllSets().get(0));
+    }
+
+    @Test
+    public void testUpdate() throws SQLException {
+        sets.addSet(set);
+        Set s = sets.getAllSets().get(0);
+        s.setContent("123Test");
+        sets.updateSet(s);
+        Set s2 = sets.getAllSets().get(0);
+        s2.setId(set.getId()); // Id has to change
+        Assert.assertEquals(s, s2);
+    }
+
+    @Test
+    public void testUpdateUnicode() throws SQLException {
+        sets.addSet(set);
+        Set s = sets.getAllSets().get(0);
+        s.setContent("\uD83D\uDE35 -- This is an emoji xoxo");
+        sets.updateSet(s);
+        Set s2 = sets.getAllSets().get(0);
+        Assert.assertEquals(s, s2);
     }
 }
