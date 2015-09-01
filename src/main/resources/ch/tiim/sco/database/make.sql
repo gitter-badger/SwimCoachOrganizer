fe6e0618bc30e5c9868d0539549613fd0e257bff
Tiim <tim.bachmann@cad.ch>
2015-09-01 15:25:51 +0200

    Fix unit test testUpdateIndexUp() in TableTrainingContentTest

--- src/main/resources/ch/tiim/sco/database/make.sql
+++ src/main/resources/ch/tiim/sco/database/make.sql
@@ -87,6 +87,6 @@ CREATE TABLE training_content (
     set_id INTEGER NOT NULL,
     indx INTEGER NOT NULL,
     FOREIGN KEY(training_id) REFERENCES training(training_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(set_id) REFERENCES sets(set_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    UNIQUE (training_id, indx)
+    FOREIGN KEY(set_id) REFERENCES sets(set_id) ON DELETE CASCADE ON UPDATE CASCADE --,
+    --UNIQUE (training_id, indx)
 );
